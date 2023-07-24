a9693145c57fe6af940ca138d802de4407bc8269
Nick Johnson <n.d.johnson@me.com>
2023-07-24 23:03:56 +0100

    now finding multiple codes reliably

--- QR-code-scanner-proto/ScanView.swift
+++ QR-code-scanner-proto/ScanView.swift
@@ -29,16 +29,21 @@ struct ScanView: View {
     @State var isShowingImage = false
     @State var scannedString:String?
     @State var triggerString:String = ""
-    @State var detectedCount = 0
     @EnvironmentObject var entry:Boats
+    @State var snapShot:UIImage?
 
     var body: some View {
         if isScanning {
-            ZStack {
-                CodeScannerView(codeTypes: [.qr], scanMode: .oncePerCode, scanInterval: 2.0, shouldVibrateOnSuccess: isVibrate, videoCaptureDevice: AVCaptureDevice(uniqueID: camera)) { response in
+            HStack {
+                //if let snapShot { Image(uiImage: snapShot) }
+                CodeScannerView(codeTypes: [.qr], scanMode: .continuous, scanInterval: 2.0, shouldVibrateOnSuccess: isVibrate, videoCaptureDevice: AVCaptureDevice(uniqueID: camera), multiCompletion: multiComplete) { response in
                     switch response {
                     case .success(let result):
-                        print("detected: \(result.string)\n")
+                        let boatId = result.string
+                        let c = result.corners
+                        let x = (c[0].x + c[1].x + c[2].x + c[3].x) / 4.0
+                        print("detected: \(boatId)\n")
+                        _ = entry.detected(id: boatId, x: x)
                         
                     case .failure(let error):
                         scannedString = error.localizedDescription
@@ -52,28 +57,41 @@ struct ScanView: View {
                     Text("Not Started")
                     ForEach(entry.boats) { boat in
                         if boat.state == .notStarted {
-                            //detectedCount = boat.track.count
                             Text("\(boat.id): \(boat.track.count)")
                         }
                     }
                     Text("Started")
                     ForEach(entry.boats) { boat in
                         if boat.state == .started {
-                            //detectedCount = boat.track.count
-                            Text("\(boat.id): \(detectedCount), \(boat.track.count)")
+                            Text("\(boat.id): \(boat.track.count)")
                         }
                     }
                     Text("Finished")
                     ForEach(entry.boats) { boat in
                         if boat.state == .finished {
-                            //detectedCount = boat.track.count
-                            Text("\(boat.id): \(boat.track.count), \(boat.finishTime)")
+                            Text("\(boat.id): \(boat.track.count)")
                         }
                     }
                 }.background(.gray.opacity(0.5))
             }
         } else {
             Text ("Preview")
+        }
+    }
+    
+    func multiComplete (_ response: Result<MultiScanResult, ScanError>) {
+        switch response {
+        case .success(let result):
+            let multiScan = result.results
+            snapShot = result.image
+            triggerString = "found \(multiScan.count) codes"
+            for scan in multiScan {
+                print ("found \(scan.qrCode), centre = \(scan.centre)")
+                _ = entry.detected(id: scan.qrCode, x: scan.centre.x)
+            }
+            
+        case .failure:
+            print("what are you doing here?")
         }
     }
 }
